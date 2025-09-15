0ab2c9ef141d3c2a6e2d61b251e70ea28cc68b16
Pete Richards <pete.richards@ksmcpa.com>
2025-09-15 14:20:26 -0400

    Add reset support to data selection groupbox

--- scripts/gui/configuration_panel/data_selection_groupbox.py
+++ scripts/gui/configuration_panel/data_selection_groupbox.py
@@ -58,6 +58,35 @@ class DataSelectionGroupbox(DisableableWidget):
         self.layout.addWidget(self.group_box)
 
 
+    def reset(self):
+        """Rebuild the group box while keeping the current filter selections."""
+        preserved_filters = {}
+        for field_items in self.box_configs.get('fields', []):
+            field_configs = widget_functions.FieldConfigs(field_items)
+            filter_key = None
+            if field_configs.data_field is not None:
+                filter_key = field_configs.data_field
+            elif field_configs.var_name is not None:
+                filter_key = field_configs.var_name
+            else:
+                filter_key = field_configs.label
+            filter_value = self.data_filter.get_filter_value(filter_key)
+            if filter_value is not None:
+                preserved_filters[filter_key] = filter_value
+
+        rebuild_enabled = self.enabled is not False
+        if rebuild_enabled:
+            self.load_ui()
+        else:
+            self.load_disabled_ui()
+
+        for filter_key, filter_value in preserved_filters.items():
+            self.data_filter.set_filter(filter_key, filter_value)
+
+        if rebuild_enabled and self.date_display_label is not None:
+            self.update_label_text()
+
+
     def update_label_text(self, delay: bool=False):
         """This function updates the label text for
         the date display label.
